80ba082852e5dc5b27c2372a599a3c9d5df55608
lehmstedt <lehmstedt-sebastien@hotmail.fr>
2016-11-08 14:57:43 +0100

    Déplacement des informations de contact du drawer à la toolbar. Intégration d'icônes pour LinkedIn et Github

--- build/bundled/service-worker.js
+++ build/bundled/service-worker.js
@@ -29,7 +29,7 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["/index.html","0db67495e7c87d3128e893ac166196d6"],["/shared-bundle.html","5dff21a6c055fc86db4840bedb9c077e"],["/src/pf-dialog/pf-dialog.html","f93f87f8f35c10a0f32b6265912c8fe2"],["/src/pf-education/pf-education.html","15c7fccae8e864ac1610d8f77591ca67"],["/src/pf-home/pf-home.html","c7be4e913d37812d4acb0ea0b9c6ce70"],["/src/pf-pro/pf-pro.html","d0e5e8e1332912f85c079af36ad4b843"],["/src/pf-projects/pf-projects.html","eb52c8f46eb9721810326ccd3a680f32"],["/src/port-folio/port-folio.html","5ce5a8f62241f20394379a2953932b2e"]];
+var PrecacheConfig = [["/index.html","0db67495e7c87d3128e893ac166196d6"],["/shared-bundle.html","5dff21a6c055fc86db4840bedb9c077e"],["/src/pf-dialog/pf-dialog.html","f93f87f8f35c10a0f32b6265912c8fe2"],["/src/pf-education/pf-education.html","15c7fccae8e864ac1610d8f77591ca67"],["/src/pf-home/pf-home.html","c7be4e913d37812d4acb0ea0b9c6ce70"],["/src/pf-pro/pf-pro.html","d0e5e8e1332912f85c079af36ad4b843"],["/src/pf-projects/pf-projects.html","eb52c8f46eb9721810326ccd3a680f32"],["/src/port-folio/port-folio.html","2e568de3fd36fbb2c472a142cd223ffa"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1--' + (self.registration ? self.registration.scope : '') + '-';
 
